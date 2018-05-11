a26eccdad8b36e431f239d789349cd733b9d4e83
LauraWest <30439638+LauraWest@users.noreply.github.com>
2018-05-11 10:05:37 +0200

    Add files via upload
    Changed to index based for loop

--- openMP_Hash_Join.cpp
+++ openMP_Hash_Join.cpp
@@ -8,6 +8,8 @@
 #include <unordered_map>
 #include <ctime>
 #include "omp.h"
+#include <typeinfo>
+#include <iterator>
 
 using namespace std;
 using tab_t = vector<vector<string>>;
@@ -40,13 +42,23 @@ tab_t Join(const tab_t& a, size_t columna, const tab_t& b, size_t columnb) {
  
     for(i = 0; i < b.size(); ++i) {
         auto range = hashmap.equal_range(b[i][columnb]);
-                for(auto it = range.first; it != range.second; ++it) 
-                {
-                        tab_t::value_type row;
-                        row.insert(row.end(), a[it->second].begin(), a[it->second].end());
-                        row.insert(row.end(), b[i].begin(), b[i].end());
-                        result.push_back(move(row));
-                }
+        //int count = std::distance(range.first, range.second);
+            for (int j = 0; j != std::distance(range.first, range.second); ++j)
+            {
+                auto it = range.first;
+                std::advance(it, j);
+                tab_t::value_type row;
+                row.insert(row.end(), a[it->second].begin(), a[it->second].end());
+                row.insert(row.end(), b[i].begin(), b[i].end());
+                result.push_back(move(row));
+            }
+            // for(auto it = range.first; it != range.second; ++it) 
+            // {
+            //         tab_t::value_type row;
+            //         row.insert(row.end(), a[it->second].begin(), a[it->second].end());
+            //         row.insert(row.end(), b[i].begin(), b[i].end());
+            //         result.push_back(move(row));
+            // }
     }
     clock_t end1 = clock();
     double elapsed_secs1 = double(end1 - begin1) / CLOCKS_PER_SEC;
@@ -55,7 +67,7 @@ tab_t Join(const tab_t& a, size_t columna, const tab_t& b, size_t columnb) {
 }
 
 int main() {
-    int numRows = 10;
+    int numRows = 7000;
     vector<string> tempRow;
     string toInsert;
     tab_t table1;
@@ -83,7 +95,8 @@ int main() {
     clock_t begin = clock(); 
     auto tab3 = Join(table1, 0, table2, 0);
     clock_t end = clock();
-    // cout << "Joined tables: " << endl << tab3 << endl;
+    //cout << "Joined tables: " << endl << tab3 << endl;
+    cout << numRows << endl;
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     cout << "Time Total: " << elapsed_secs << endl;
 
