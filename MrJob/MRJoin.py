f16c74ac2f721191b477df4ad3052c84824740b3
Vorapahct <iordtchap@gmail.com>
2018-05-12 15:46:32 +0200

    Added fixes for multiple keys

--- MrJob/MRJoin.py
+++ MrJob/MRJoin.py
@@ -21,10 +21,10 @@ class MRJoin(MRJob):
 				value = data[x]
 				hash_key = hashlib.md5(value[0]).hexdigest()
 				
-				if filename == sys.argv[-2]:
-					yield hash_key, value
+				if filename == sys.argv[1]:
+					yield hash_key, (value, 'A')
 				else:
-					yield hash_key, value		
+					yield hash_key, (value, 'B')		
 				
 		def reducer_1(self, hash_key, values):
 			output = []
@@ -33,17 +33,32 @@ class MRJoin(MRJob):
 			if num_values == 2:
 				for num_val in values_list:
 					for i in num_val:
-						output.append(i)
+						if i != 'A' and i != 'B':
+							
+							print(i[:])
+							output.append(i[:])
 				yield None, output	
 				
 			elif num_values%2 == 0:
-				print(values_list)
 				A = values_list[:len(values_list)/2]
 				B = values_list[len(values_list)/2:]
-				
 				for y in range(0,len(A)):
 					for x in range(0,len(B)):
-						temp = A[y] + B[x]
+						temp = [A[y][0]] + [B[x][0]]
 						yield None, temp
+			elif num_values%2 == 1:
+				count = 0;
+				A = []
+				B = []
+				for j in values_list:
+					if j[1] == 'A':
+						A.append(j[0])
+					else:
+						B.append(j[0])
+				for y in range(0,len(A)):
+					for x in range(0,len(B)):
+						temp = [A[y]] + [B[x]]
+						yield None, temp
+
 			
 MRJoin.run()
