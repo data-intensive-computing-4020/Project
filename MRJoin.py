a7fe3a03d5a546845d3f611e4c516b34634f677f
Vorapahct <iordtchap@gmail.com>
2018-05-15 15:30:28 +0200

    MRJoin comments added

--- MRJoin.py
+++ MRJoin.py
@@ -1,3 +1,7 @@
+# The MapReduce reduce-side join algorithm
+# Input execution command: python MRJoin.py <table 1 in json format> <x> <table 2 in json format> <x> <join output file> <Benchmark time output file>
+# Where x and y are the attribute to join with
+
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 from prettytable import PrettyTable
@@ -7,12 +11,13 @@ import sys
 import time
 import json
 
-
 class MRJoin(MRJob):
 
+    # This defines the steps the job will follow 
     def steps(self):
         return [MRStep(mapper=self.map_keys, reducer=self.reduce_keys), MRStep(reducer=self.reduce_for_output)]
 
+    # This is the mapper phase that produces the key-value pairs
     def map_keys(self, _, line):
         filename = os.environ['map_input_file']
         table = json.loads(line)
@@ -25,10 +30,12 @@ class MRJoin(MRJob):
                 key = str(record[join_field_2])
                 yield key, (record, 'B')
 
+    # This is the reducer phase that joins the records from the table
     def reduce_keys(self, key, records):
         output = []
         records_list = list(records)
         num_records = len(records_list)
+        # Simple join of matching records
         if num_records == 2:
             for num_val in records_list:
                 for i in num_val:
@@ -36,7 +43,8 @@ class MRJoin(MRJob):
                         output.append(i)
             output = (output[0], output[1])
             yield None, output
-
+				
+	# Join of records when there are multiple values in both tables corresponding to the same key
         elif num_records % 2 == 0:
             A = records_list[:len(records_list) / 2]
             B = records_list[len(records_list) / 2:]
@@ -44,7 +52,8 @@ class MRJoin(MRJob):
                 for x in B:
                     joined_value = (y[0], x[0])
                     yield None, joined_value
-
+				
+        # Join of records when there is one value in one table and multiple values in another correspondning to the same key
         elif num_records % 2 == 1:
             count = 0;
             A = []
@@ -59,6 +68,7 @@ class MRJoin(MRJob):
                     joined_value = (A, B)
                     yield None, joined_value
 
+    # This is a reducer phase for displaying all the records in the resultant table in a clear json format		
     def reduce_for_output(self, _, records):
         output = []
         for val in records:
@@ -67,7 +77,7 @@ class MRJoin(MRJob):
         global final_output
         final_output = output
 
-
+# Extraction of arguments from the command line
 command_line = sys.argv
 
 join_field_1 = int(command_line[2])
@@ -75,21 +85,23 @@ join_field_2 = int(command_line[4])
 output_file_name = command_line[5]
 benchmark_file_name = command_line[6]
 
-# Laura's sys.argv
-# sys.argv = [command_line[0], command_line[1], command_line[3], command_line[6]]
 sys.argv = [command_line[0], command_line[1], command_line[3]]
 
+# Creation of the output benchmark table for the timing results
 final_output = []
 time_table = PrettyTable()
 time_table.field_names = ["Benchmark", "Time taken"]
 
+# Timing of the algorithm
 start_time = time.time()
 MRJoin.run()
 end_time = time.time()
 total_time = end_time - start_time
 
+# Addition of the timed result to the output benchmark table
 time_table.add_row(["Total time", str(total_time)])
 
+# Writing an output text file
 time_write = open(benchmark_file_name, 'a+')
 time_write.write("***MRJoin Benchmark Results*** \n")
 time_write.write(str(time_table))
